74b902ece96d7a864f7e93163eed29855b6b0be4
Bassma Ennarah <123920913+bennarah@users.noreply.github.com>
2025-06-06 23:34:21 +0000

    updated

--- DirectionsAlgo.py
+++ DirectionsAlgo.py
@@ -1,1 +1,49 @@
+def dijkstra(graph, start):
+    shortest_time = {}
+    previous_place = {}
+    unseen = list(graph.keys())
 
+    for place in unseen:
+        shortest_time[place] = float('inf')
+    shortest_time[start] = 0
+
+    while unseen:
+        current = min(unseen, key=lambda place: shortest_time[place])
+        unseen.remove(current)
+
+        for neighbor, time in graph[current]:
+            new_time = shortest_time[current] + time
+            if new_time < shortest_time[neighbor]:
+                shortest_time[neighbor] = new_time
+                previous_place[neighbor] = current
+
+    return shortest_time, previous_place
+
+def get_path(previous_place, start, end):
+    path = []
+    current = end
+    while current != start:
+        if current is None:
+            return ["No path found"]
+        path.append(current)
+        current = previous_place.get(current)
+    path.append(start)
+    return path[::-1]
+
+graph = {
+    'CSUF': [('57 Freeway', 5), ('Sand Canyon', 60)],
+    '57 Freeway': [('5 Freeway', 10)],
+    '5 Freeway': [('Sand Canyon', 7)],
+    'Sand Canyon': [('Bassma\'s Home', 5)],
+    'Bassma\'s Home': []
+}
+
+if __name__ == "__main__":
+    start = 'CSUF'
+    end = 'Bassma\'s Home'
+
+    times, previous = dijkstra(graph, start)
+    path = get_path(previous, start, end)
+
+    print("Estimated Travel Time:", times[end], "minutes")
+    print("Directions:", " → ".join(path))
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # DrivingDirectionsAlgo
+
+# dijkstra's algo from textbook
+
+def dijkstra(G, s):
+distance = Vector(G.vertex_count(), None)
+penultimate = Vector(G.vertex_count(), None)
+seen = Vector(G.vertex_count(), False)
+distance[s.index] = 0
+seen[s.index] = True
+done = False
+while not done:
+<FIND SOME EDGE b={v, u} SUCH THAT v IS SEEN AND u IS UNSEEN, AND
+[s, ..., v, u]
+IS A CORRECT SHORTEST PATH TO u>
+if <NO SUCH b EXISTS>:
+done = True
+else:
+distance[u.index] = distance[v.index] + b.label
+penultimate[u.index] = v
+seen[u.index] = True
+return distance, penultimate
+148
